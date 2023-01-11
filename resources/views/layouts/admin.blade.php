5d3526d207d6f0b9e31b402a9ba828a76d7d056a
UniversoCaotico1997 <loria.roberto97@gmail.com>
2023-01-11 16:09:22 +0100

    refactoring scss

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -26,6 +26,7 @@
             <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+            <input class="form-control form-control-dark w-100 rounded-0 border-0" type="text" placeholder="Search" aria-label="Search">
             <div class="navbar-nav">
                 <div class="nav-item text-nowrap">
                     <a class="nav-link px-3" href="#">Sign out</a>
@@ -46,8 +47,8 @@
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="#">
-                                    <span data-feather="shopping-cart" class="align-text-bottom"></span>
-                                    Products
+                                    <span data-feather="file" class="align-text-bottom"></span>
+                                    Project
                                 </a>
                             </li>
                         </ul>
@@ -59,6 +60,7 @@
                 </main>
             </div>
         </div>
+
 
     </div>
 </body>
